914e753a931e38fa4bae9eae6ccb96acd50f440a
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-12-07 12:15:53 -0700

    Lab 4 complete

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,9 +1,22 @@
 import { Button, TextInput, View, StyleSheet } from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import tasksList from "../data/tasks.json";
 
 export default function ToDoForm({addTask}){
   
   const [taskText, setTaskText] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=> {
+    setTasks(tasksList.tasks);
+  },[]);
+
+  const handleRandomTask = () =>{
+    if (tasks.length > 0){
+      const random = tasks[Math.floor(Math.random() *tasks.length)];
+      setTaskText(random);
+    }
+  }
   
   return(
     <View style={styles.form}>
@@ -13,6 +26,7 @@ export default function ToDoForm({addTask}){
         onChangeText={(text) => setTaskText(text)}
         value={taskText}
       />
+      <Button title="Generate Random Task" onPress={handleRandomTask}/>
       <Button title="Add Task" onPress={() => addTask(taskText)} />
 
         </View>
